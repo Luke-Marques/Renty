a38e39fc22dd3ede5664a65f4f18e78d9eeda152
Archie <archieatkinson97@gmail.com>
2021-06-27 22:16:08 +0100

    fixed bug in furnished filter

--- url_builder.py
+++ url_builder.py
@@ -5,6 +5,7 @@ from enum import Enum
 class SortType(Enum):
     def __str__(self):
         return str(self.value)
+
     OLDEST_LISTED = 10
     NEWEST_LISTED = 0
     HIGHEST_PRICED = 2
@@ -19,7 +20,7 @@ class RegionIndex(Enum):
 
 
 class URLBuilder:
-    base_url = "https://www.rightmove.co.uk/property-for-sale/find.html?"
+    base_url = "https://www.rightmove.co.uk/property-to-rent/find.html?"
     region_selected = False
     allowed_prices = [
         100, 150, 200, 250, 300, 350, 400, 450, 500, 600, 700, 800, 900, 1000, 1100, 1200,
@@ -30,7 +31,8 @@ class URLBuilder:
     allowed_furnish = ["furnished", "partFurnished", "unfurnished"]
     allowed_dont_show = ["houseShare", "retirement", "student"]
     allowed_must_have = ["student", "houseShare", "garden", "retirement", "parking"]
-    allowed_properties = ["bungalow", "detached", "flat", "land", "park-home", "private-halls", "semi-detached", "terraced"]
+    allowed_properties = ["bungalow", "detached", "flat", "land", "park-home", "private-halls", "semi-detached",
+                          "terraced"]
 
     def __init__(self):
         self.url = URLBuilder.base_url
@@ -40,11 +42,11 @@ class URLBuilder:
         return self
 
     def min_bedrooms(self, no_bedrooms):
-        self.url += "&maxBedrooms=" + str(no_bedrooms)
+        self.url += "&minBedrooms=" + str(no_bedrooms)
         return self
 
     def max_bedrooms(self, no_bedrooms):
-        self.url += "&minBedrooms=" + str(no_bedrooms)
+        self.url += "&maxBedrooms=" + str(no_bedrooms)
         return self
 
     def property_type(self, *argv):
@@ -56,7 +58,7 @@ class URLBuilder:
         return self
 
     def furnished_type(self, *argv):
-        self.url += "&furnishTypes="
+        self.url += "&furnishType="
         for arg in argv:
             if arg not in self.allowed_furnish:
                 raise ValueError('Error! Furnished type "' + str(arg) + '" not allowed')
@@ -87,17 +89,21 @@ class URLBuilder:
     def min_price(self, price_per_month):
         if price_per_month not in self.allowed_prices:
             raise ValueError('Error! Price "' + str(price_per_month) + '" not allowed')
-        self.url += "&minPrice==" + str(price_per_month)
+        self.url += "&minPrice=" + str(price_per_month)
         return self
 
     def max_price(self, price_per_month):
         if price_per_month not in self.allowed_prices:
             raise ValueError('Error! Furnished type "' + str(price_per_month) + '" not allowed')
-        self.url += "&maxPrice==" + str(price_per_month)
+        self.url += "&maxPrice=" + str(price_per_month)
         return self
 
     def show_let_agreed(self, show_let_agreed):
-        self.url += "&includeLetAgreed " + str(show_let_agreed)
+        self.url += "&includeLetAgreed=" + str(show_let_agreed)
+        return self
+
+    def set_page(self, page):
+        self.url += "&index=" + str(page * 24)
         return self
 
     def get_result(self):
@@ -110,17 +116,14 @@ class URLSets:
     """The Director, building a complex representation."""
 
     @staticmethod
-    def standard():
+    def standard(page_no=0):
         """Constructs and returns the final product"""
         return URLBuilder() \
+            .set_page(page_no) \
             .sort_type(SortType.HIGHEST_PRICED) \
             .min_bedrooms(0) \
             .max_bedrooms(0) \
             .furnished_type("partFurnished", "unfurnished") \
-            .must_have("garden", "parking") \
-            .dont_show("retirement") \
-            .region(RegionIndex.BRISTOL) \
-            .min_price(100) \
             .get_result()
 
 
